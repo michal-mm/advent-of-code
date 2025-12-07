24d9cd82895c070f9b80103ef8cf3e57e1ef5718
michal-mm <michal.miotk@gmail.com>
2025-12-07 21:34:00 +0100

    Day #7, exercise 2- brute force, very innefficient.

---
+++ src/aoc2025/D07_2.java
@@ -0,0 +1,104 @@
+package aoc2025;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class D07_2 {
+
+    private static final String INPUT_FILE = "resources/aoc2025/D07_2_input.txt";
+
+    void main() throws IOException {
+        var input = Files.readString(Path.of(INPUT_FILE));
+        var grid = parseInput(input);
+        var solution = getNumOfDifferentRoutes(grid);
+        IO.println("Solution: " + solution);
+    }
+
+    @Test
+    void testExample() {
+        var input = """
+                .......S.......
+                ...............
+                .......^.......
+                ...............
+                ......^.^......
+                ...............
+                .....^.^.^.....
+                ...............
+                ....^.^...^....
+                ...............
+                ...^.^...^.^...
+                ...............
+                ..^...^.....^..
+                ...............
+                .^.^.^.^.^...^.
+                ...............
+                """;
+
+        var grid = parseInput(input);
+        var expected = 40;
+
+        var solution = getNumOfDifferentRoutes(grid);
+
+        assertEquals(expected, solution);
+
+    }
+
+    int getNumOfDifferentRoutes(List<List<Character>> grid) {
+        Set<List<Integer>> routes = new HashSet<>();
+
+        var startIdx = grid.getFirst().indexOf('S');
+        List<Integer> path = new ArrayList<>();
+        path.add(startIdx);
+
+        var rowIdx = 1;
+        explorePaths(grid, routes, path, rowIdx, startIdx);
+
+        return routes.size();
+    }
+
+    List<List<Character>> parseInput(String input) {
+        List<List<Character>> grid = new ArrayList<>();
+
+        var scanner = new Scanner(input);
+        while(scanner.hasNextLine()) {
+            List<Character> row = new ArrayList<>();
+            grid.add(row);
+            for (char c : scanner.nextLine().toCharArray()) {
+                row.add(c);
+            }
+        }
+
+        return grid;
+    }
+
+    private void explorePaths(List<List<Character>> grid,
+                              Set<List<Integer>> routes,
+                              List<Integer> path, int row, int index) {
+        if (row == grid.size()-1) {
+            routes.add(path);
+            return;
+        }
+
+        if (grid.get(row).get(index) == '^') {
+            // go left
+            path.add(index-1);
+            explorePaths(grid, routes, path, row+1, index-1);
+            path.removeLast();
+            // now go right
+            path.add(index+1);
+            explorePaths(grid, routes, path, row+1, index+1);
+            path.removeLast();
+        } else {
+            path.add(index);
+            explorePaths(grid, routes, path, row+1, index);
+            path.removeLast();
+        }
+    }
+}
